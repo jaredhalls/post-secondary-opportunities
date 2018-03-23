fbc13189d8d4dc4dea3e7a7921fbd364bda74a11
Michael <mlam1@ocdsb.ca>
2018-03-23 11:49:53 -0400

    Reduced bandwidth, added posts, renamed classes & ids, cleaned CSS code.

--- collapsible.js
+++ collapsible.js
@@ -1,4 +1,4 @@
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < acc.length; i++) {
--- stickynavbar.js
+++ stickynavbar.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {
   myFunction()
 };
-var header = document.getElementById("myHeader");
+var header = document.getElementById("navHeader");
 var sticky = header.offsetTop;
 
 function myFunction() {
